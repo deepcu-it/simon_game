b9beb23dfaa7e81e142e04d99cf281b342d70a98
deepcu-it <130172923+deepcu-it@users.noreply.github.com>
2023-06-03 13:53:07 +0530

    Update game.js

--- game.js
+++ game.js
@@ -57,7 +57,7 @@ function animatepress(currentcolor) {
     }, 100);
 }
 function playsound(name) {
-    var audio=new Audio("sounds/"+name+".mp3");
+    var audio=new Audio(name+".mp3");
     audio.play();
 }
 function startOver() {
